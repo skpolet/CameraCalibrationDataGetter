75545af665e1a0474a9d24ed017d56a33c93d3ca
Sergey Mikhailov <sergey.mikhailov@customertimes.com>
2022-04-22 16:14:28 +0200

    Вынес provider отдельно

--- CameraCalibrationDataGetter/ViewController.swift
+++ CameraCalibrationDataGetter/ViewController.swift
@@ -6,8 +6,6 @@
 //
 
 import UIKit
-import VideoToolbox
-import ARKit
 
 class ViewController: UIViewController {
     
@@ -20,56 +18,4 @@ class ViewController: UIViewController {
         CameraProvider().setupCameraSession(with: cameraView.cameraLayer)
     }
 
-}
-
-extension CameraProvider: AVCapturePhotoCaptureDelegate, AVCaptureVideoDataOutputSampleBufferDelegate {
-    
-    public func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        
-        if let camData = CMGetAttachment(sampleBuffer, key:kCMSampleBufferAttachmentKey_CameraIntrinsicMatrix, attachmentModeOut:nil) as? Data {
-            let matrix: matrix_float3x3 = camData.withUnsafeBytes { $0.pointee }
-            print(matrix)
-            // > simd_float3x3(columns: (SIMD3<Float>(1599.8231, 0.0, 0.0), SIMD3<Float>(0.0, 1599.8231, 0.0), SIMD3<Float>(539.5, 959.5, 1.0)))
-        }
-    }
-}
-
-public class CameraProvider: NSObject {
-    
-    private let session: AVCaptureSession = {
-        let session = AVCaptureSession()
-        session.sessionPreset = .high
-        return session
-    }()
-    
-    private let videoOutput: AVCaptureVideoDataOutput = {
-        let videoOutput = AVCaptureVideoDataOutput()
-        videoOutput.connection(with: .video)?.isCameraIntrinsicMatrixDeliveryEnabled = true
-        return videoOutput
-    }()
-    
-    public func setupCameraSession(with layer: AVCaptureVideoPreviewLayer) {
-        guard let captureDevice = AVCaptureDevice.default(for: .video) else {
-            return
-        }
-        
-        session.beginConfiguration()
-        do {
-            let input = try AVCaptureDeviceInput(device: captureDevice)
-            if session.canAddInput(input) {
-                session.addInput(input)
-                
-                layer.session = session
-                layer.videoGravity = .resizeAspectFill
-            }
-        } catch let error {
-            debugPrint("Failed to set input device with error: \(error)")
-        }
-        
-        if session.canAddOutput(videoOutput) {
-            session.addOutput(videoOutput)
-        }
-        session.commitConfiguration()
-        session.startRunning()
-    }
 }
